dbbbe8fbde23e29777049db713726733ab949246
Jayllian Marquardt <jayllian.marquardt.2021@student.lhschools.org>
2020-02-20 13:36:14 -0500

    called Hello World Method

--- src/GithubHelloWorld.java
+++ src/GithubHelloWorld.java
@@ -1,11 +1,12 @@
 public class GithubHelloWorld {
     public static void main(String[] args) {
     System.out.println("Jayllian");
-        
+    HelloWorld();
     }
 
-    public static void helloWorld(){
-        System.out.println("Hello World");
+    public static void HelloWorld(){
+
+        System.out.println("Hello Github World");
     }
 
 
